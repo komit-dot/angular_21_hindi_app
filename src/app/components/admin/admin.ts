e48b4f1cb230a3fc8ead3bd7abc134c800bafa4e
komit-dot <komitcha@gmail.com>
2026-07-15 12:59:24 +0530

    ingerate component lifecycle hook

--- src/app/components/admin/admin.ts
+++ src/app/components/admin/admin.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
-export class Admin {
+export class Admin implements OnInit,AfterViewInit, OnDestroy  {
+
+  constructor(){
+    console.log('Hello this is constructor')
+    // property initialize
+  }
+
+  ngOnInit(){
+    console.log('this is ngoninit')
+  }
+  //apicall, subscription
+
+  ngAfterViewInit(): void {
+    console.log('ngAftervieinit')
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngondestroyyyy')
+  }
 
 }
