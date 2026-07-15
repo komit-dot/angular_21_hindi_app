d1c89277a4a91b1cb53b23bed78b639dd7bd6a2e
komit-dot <komitcha@gmail.com>
2026-07-15 17:55:10 +0530

    all changes for data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataBinding } from "./components/data-binding/data-binding";
+import { Signal } from "./components/signal/signal";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Signal],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
