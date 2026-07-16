e06d7370494619ad319a303ac43f8fb5512a0182
komit-dot <komitcha@gmail.com>
2026-07-16 16:51:00 +0530

    deleted signal.html and signal.ts file

---
+++ src/app/components/signal/signal.ts
@@ -0,0 +1,32 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signal',
+  imports: [],
+  templateUrl: './signal.html',
+  styleUrl: './signal.css',
+})
+export class Signal {
+  courseName: string = 'Angular';
+
+  angularVersion = signal('20');
+  courseDuration = signal<string>('2 Months');
+  cityList = signal<string[]>(['Pune', 'Mumbai']);
+  studentobj = signal<any>({
+    name: 'harsh',
+    city: 'Lucknow',
+  });
+
+  coursechange() {
+    this.courseDuration.set('32 Months');
+    this.courseName = 'React.js';
+  }
+
+  addCity(cityName: string) {
+    this.cityList.update((old: string[]) => [...old, cityName]);
+  }
+
+  changeCity() {
+    this.studentobj.update((oldObj: any) => ({ ...oldObj, city: 'Ghaziabad' }));
+  }
+}
