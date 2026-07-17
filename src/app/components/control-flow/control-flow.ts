2f24bce3c92774ba943687551508b3d24265eb07
komit-dot <komitcha@gmail.com>
2026-07-17 12:03:57 +0530

    integrate boot strap into all project

--- src/app/components/control-flow/control-flow.ts
+++ src/app/components/control-flow/control-flow.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -10,6 +10,8 @@ import { FormsModule } from '@angular/forms';
 export class ControlFlow {
 
   selectedCaste:string = '';
+
+  isStudentActive = signal<boolean> (true)
 
 
 
