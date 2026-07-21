50322d0e2e0663f26ff7c9f728f44fdd8721711b
komit-dot <komitcha@gmail.com>
2026-07-21 11:43:04 +0530

    angular routes works finished here

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { ControlFlow } from './components/control-flow/control-flow';
+import { DataBinding } from './components/data-binding/data-binding';
+import { Signal } from './components/signal/signal';
+import { Attribute } from './components/attribute/attribute';
+import { User } from './components/user/user';
+import { Admin } from './components/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      redirectTo: 'data-binding',
+      pathMatch: 'full'
+    },
+    {
+      path: 'controlflow',
+      component: ControlFlow
+    },
+    {
+      path: 'user',
+      component: User
+    },
+    {
+      path: 'admin',
+      component: Admin
+    },
+    {
+      path: 'data-binding',
+      component: DataBinding
+    },
+    {
+      path: 'signal',
+      component: Signal
+    },
+    {
+      path: 'attribute',
+      component: Attribute
+    }
+];
+
