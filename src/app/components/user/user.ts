ac163ebb14491e178f1bc6cb522484b1ae93aebf
komit-dot <komitcha@gmail.com>
2026-07-28 19:46:10 +0530

    user works start for reactive form

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
+  userForm: FormGroup = new FormGroup({
+    userId: new FormControl(0),
+    emailId: new FormControl(''),
+    password: new FormControl(''),
+    fullName: new FormControl(''),
+    mobileNo: new FormControl(''),
+
+  });
+
+  onSaveUser(){
+    
+  }
+  
+    
+  
 
 }
